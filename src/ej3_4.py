7ade0fef57bd8f2d882574204c31be1d4232af4a
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2023-11-15 19:55:24 +0100

    estudio

--- src/ej3_4.py
+++ src/ej3_4.py
@@ -1,17 +1,22 @@
 def pedirnum(numeros: list):
-    numeros = numeros.
-    if
+    cont = 1
+    while cont <= 6:
+        num=input(f"{cont} =>")
+        if num <= 0 or num  >= 50 : 
+            print("Valor Invalido")
+    
 
 
 
-def orden(numeros):
+def orden(numeros : list):
     numeros = numeros.sort()
+    numeros = numeros.reverse()
     return numeros
 
 def main():
     print("Ingrese los numeros : ")
-    numeros = list("")
-    numeros = 
+    numeros = list()
+    pedirnum(numeros)
     print(orden(numeros))
 
 if __name__ == "__main__":
