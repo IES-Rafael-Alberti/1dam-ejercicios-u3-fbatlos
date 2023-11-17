e26b6976d33a8cc2b2aca426394d90bec5e55b9e
fbatlos123 <bdlsfrancisco@iesrafaelalberti.es>
2023-11-17 12:07:43 +0100

    estudia

---
+++ src/ej3_5.py
@@ -0,0 +1,10 @@
+def reversa(numeros : list):
+    numeros.reverse()
+    print (numeros)
+
+def main():
+    numeros = ([1,2,3,4,5,6,7,8,9,10])
+    reversa(numeros)
+
+if __name__ == "__main__":
+    main()
