3d4acaecb8dd6be857cb4220bc7c1df9085423af
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2023-11-20 20:17:25 +0100

    estudia

---
+++ src/ej3_2_1.py
@@ -0,0 +1,11 @@
+def pedirdivisa(divisa):
+    dentro = input("Dime tu divisa entre : Euro , Dollar y Yen : ")
+    while dentro not in divisa : 
+        print("**ERROR** Divisa no encontrada , prueba otra vez ")
+        dentro = input("=> ")
+    print(divisa[dentro]) 
+def main():
+    divisa = {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
+    pedirdivisa(divisa)
+if __name__ == "__main__":
+    main()
