73c217cd0bc10e11b0547d0439f9a144fcfd54e1
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2023-11-20 21:31:08 +0100

    estudia

---
+++ src/ej3_3_1.py
@@ -0,0 +1,17 @@
+def domicilios_clientes(lista_compras):
+    domicilio = set()
+    for compra in lista_compras:
+        domicilio.add(compra[3])
+    return domicilio
+
+
+
+
+def main():
+    lista_compras = [()]
+    dimicilios = domicilios_clientes(lista_compras)
+
+    
+    
+if __name__ == "__main__":
+    main()
