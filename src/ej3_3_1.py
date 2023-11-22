1d8072ca6d2952369b5aa35cd72f5adfcca255bd
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2023-11-22 19:19:07 +0100

    estudia

--- src/ej3_3_1.py
+++ src/ej3_3_1.py
@@ -1,16 +1,12 @@
-def domicilios_clientes(lista_compras):
-    domicilio = set()
-    for compra in lista_compras:
-        domicilio.add(compra[3])
-    return domicilio
-
+def comprobar1(clientes) -> set:
+    return set(clientes[3] for i in range (len(clientes)))
 
 
 
 def main():
-    lista_compras = [()]
-    dimicilios = domicilios_clientes(lista_compras)
-    
+    clientes = [("Nuria Costa", 5, 12780.78, "Calle Las Flores 355"), ("Jorge Russo", 7, 699, "Mirasol 218"), ("Nuria Costa", 7, 532.90, "Calle Las Flores 355"), ("Julián Rodriguez", 12, 5715.99, "La Mancha 761"), ("Jorge Russo", 15, 958, "Mirasol 218")]
+    conjunto = comprobar1(clientes)
+    print(conjunto)
     
 if __name__ == "__main__":
     main()
